d89286f4f6ae06849f2481f2ec2167145f3e5a7c
aachal01 <95211796+aachal01@users.noreply.github.com>
2024-04-01 00:53:28 +0530

    test: testing using vitest with hardhat (#20)
    * feat: testing using vitest with hardhat
    
    * space hataya
    
    * blah blah
    
    * ci: added build and test scripts
    
    * ci: some changes
    
    * chore: space dala
    
    * test: formated test files
    
    * chore: commented NX env variables
    
    * chore: updated nx env variables
    
    * chore: updated nx env variables
    
    * chore: formated database export
    
    * fix: open db before each operation
    
    * refactor: updated format
    
    * chore: updated nx version
    
    * fix: updated pnpm lock file
    
    * chore: undo nx version change
    
    * fix: create new database no reuse
    
    ---------
    
    Co-authored-by: Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>

--- packages/database/src/core/Storage.ts
+++ packages/database/src/core/Storage.ts
@@ -1,38 +1,45 @@
-import { type Level } from 'level'
+import { Level } from 'level'
 import type { IDatabase, IDatabaseItemType } from '@jhasuraj01/interface'
 import { serialize, deserialize } from '@jhasuraj01/utils'
+import { databasePrefix } from './constants'
 
 export class Storage<T extends IDatabaseItemType> implements IDatabase<T> {
   private readonly db
 
-  constructor(
-    db: Level,
-    readonly name: string
-  ) {
-    this.db = db.sublevel<string, T>(name, { valueEncoding: 'json' })
+  constructor(readonly name: string) {
+    this.db = new Level<string, T>(databasePrefix + name, {
+      valueEncoding: 'json',
+      version: 1
+    })
   }
 
   async writeOne(data: T): Promise<void> {
+    await this.db.open()
     await this.db.put(data.id, serialize(data) as T)
   }
 
   async findOneById(id: T['id']): Promise<T> {
+    await this.db.open()
     return (await this.db.get(id).then(deserialize)) as T
   }
 
   async deleteOneById(id: T['id']): Promise<void> {
+    await this.db.open()
     await this.db.del(id)
   }
 
   async getAll(): Promise<T[]> {
+    await this.db.open()
     return (await this.db.values().all()).map(deserialize) as T[]
   }
 
   async getAllIds(): Promise<Array<T['id']>> {
+    await this.db.open()
     return await this.db.keys().all()
   }
 
   async clear(): Promise<void> {
+    await this.db.open()
     await this.db.clear()
   }
 }
