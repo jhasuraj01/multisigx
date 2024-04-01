ee0fd0d69b798aa66898b4d4efd03c87abf66a32
Vedang Dadape <81163049+Vedang12d@users.noreply.github.com>
2024-04-01 07:10:47 +0000

    feat: add GraphFactory and GraphContract contracts

---
+++ packages/parser/contracts/1_GraphFactory.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+import './2_GraphContract.sol';
+
+contract GraphFactory {
+  constructor() {}
+  function build(
+    string[] memory ids,
+    string[] memory names,
+    string[][] memory dependentsArray,
+    string[][] memory dependsOnArray,
+    string[] memory types,
+    string[] memory internalLogicArray,
+    string[] memory addressArray,
+    int[] memory thresholdArray
+  ) public returns (address) {
+    GraphContract graph = new GraphContract(
+      ids,
+      names,
+      dependentsArray,
+      dependsOnArray,
+      types,
+      internalLogicArray,
+      addressArray,
+      thresholdArray
+    );
+    return address(graph);
+  }
+}
