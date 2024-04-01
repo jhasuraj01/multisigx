5688038f9bcb85d461d6d7609b34acc1a2270a63
Vedang Dadape <81163049+Vedang12d@users.noreply.github.com>
2024-04-01 06:55:25 +0000

    feat: add rule graph compiler

---
+++ packages/composer/src/rule-graph-compiler.ts
@@ -0,0 +1,106 @@
+import {
+  RuleGraphObject,
+  type Rule
+} from '@jhasuraj01/interface';
+
+interface RuleGraphCompiledObject {
+  ids: string[];
+  names: string[];
+  dependentsArray: string[][];
+  dependsOnArray: string[][];
+  types: string[];
+  internalLogicArray: string[];
+  addressArray: string[];
+  thresholdArray: number[];
+}
+
+export const compile = (_graphObject: RuleGraphObject): RuleGraphCompiledObject => {
+  const ids: string[] = [];
+  const names: string[] = [];
+  const dependentsArray: string[][] = [];
+  const dependsOnArray: string[][] = [];
+  const types: string[] = [];
+  const internalLogicArray: string[] = [];
+  const addressArray: string[] = [];
+  const thresholdArray: number[] = [];
+
+  const rules: Rule[] = Array.from(_graphObject.rules.values());
+
+  rules.forEach((rule) => {
+    const id = rule?.id;
+    const name = rule?.name;
+    const type = rule?.type;
+    let dependents: string[] = [];
+    if(type !== 'END') {
+      dependents = Array.from(rule.dependents.values());
+    }
+    let dependsOn: string[] = [];
+    if(type !== 'START') {
+      dependsOn = Array.from(rule.dependsOn.values());
+    }
+    let internalLogicType: string = '';
+    if(type === 'SIGN' || type === 'END') {
+      internalLogicType = rule?.internalLogic?.type;
+    }
+    let address: string = '';
+    if(type === 'SIGN') {
+      address = rule?.address;
+    }
+
+    let threshold;
+
+    switch (type) {
+      case 'SIGN':
+        if (rule?.internalLogic?.type === 'AND')
+          threshold = dependsOn.length;
+        else if (rule?.internalLogic?.type === 'OR')
+          threshold = 1;
+        else
+          threshold = rule?.internalLogic?.count;
+        break;
+      case 'END':
+        if (rule?.internalLogic?.type === 'AND')
+          threshold = dependsOn.length;
+        else if (rule?.internalLogic?.type === 'OR')
+          threshold = 1;
+        else
+          threshold = rule?.internalLogic?.count;
+        break;
+      case 'AND':
+        threshold = dependsOn.length;
+        break;
+      case 'OR':
+        threshold = 1;
+        break;
+      case 'ATLEAST':
+        threshold = rule?.count;
+        break;
+      case 'START':
+        threshold = 0;
+        break;
+      default:
+        threshold = -1;
+        throw new Error('Invalid Rule');
+    }
+
+    ids.push(id);
+    names.push(name);
+    dependentsArray.push(dependents);
+    dependsOnArray.push(dependsOn);
+    types.push(type);
+    internalLogicArray.push(internalLogicType);
+    addressArray.push(address);
+    thresholdArray.push(threshold);
+  });
+
+  return {
+    ids,
+    names,
+    dependentsArray,
+    dependsOnArray,
+    types,
+    internalLogicArray,
+    addressArray,
+    thresholdArray
+  };
+}
